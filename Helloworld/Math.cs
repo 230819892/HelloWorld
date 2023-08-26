787566830c05b1da8b39cb460e46c0dce9485a7c
Lenovo <Lenovo@DESKTOP-R9ESODS>
2023-08-26 11:05:12 +0200

    Add project files.

---
+++ Helloworld/Math.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace HelloWorld
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+
+}
